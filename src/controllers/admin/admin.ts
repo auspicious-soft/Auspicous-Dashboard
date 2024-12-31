3c2318f68b44c44ccdc1efa86395e9f85e0cd61d
Harish <harish.auspicioussoft@gmail.com>
2024-12-31 11:32:21 +0530

    test

--- src/controllers/admin/admin.ts
+++ src/controllers/admin/admin.ts
@@ -198,7 +198,7 @@ export const createtarget = async (req: Request, res: Response) => {
 export const updatetarget = async (req: Request, res: Response) => {
     try {
         const response = await updatetargetService({ currentUser: (req as any).currentUser, ...req.body }, res)
-        return res.status(httpStatusCode.CREATED).json(response)
+        return res.status(httpStatusCode.OK).json(response)
     } catch (error: any) {
         const { code, message } = errorParser(error)
         return res.status(code || httpStatusCode.INTERNAL_SERVER_ERROR).json({ success: false, message: message || "An error occurred" });
